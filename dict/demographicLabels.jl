38e2052fbf168b8f05b7362b85b2aff91d139806
Pedro Ribeiro de Almeida <pedroribalmeida@gmail.com>
2023-09-30 23:42:26 +1000

    Refactor DEMOGRAPHICLABELS to follow styleguide

--- dict/demographicLabels.jl
+++ dict/demographicLabels.jl
@@ -1,6 +1,1 @@
-const DEMOGRAPHICLABELS = [
-  "0 - 14 years", 
-  "15 - 24 years", 
-  "25 - 64 years", 
-  "+65 years"
-]
+const DEMOGRAPHICLABELS = ["0 - 14 years", "15 - 24 years", "25 - 64 years", "+65 years"]
